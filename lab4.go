a3908acf92e560098f00d81f441eadb9f6c19e08
MishaKazakov <33093997+MishaKazakov@users.noreply.github.com>
2018-01-09 20:56:23 +0700

    Add files via upload
    done

--- lab4.go
+++ lab4.go
@@ -12,48 +12,49 @@ type Token struct {
 	ttl       int
 }
 
-// call go run lab4.go 15 "message",
+// call from terminal  go run lab4.go msg recipient_num lifetime number_of_elements
 func main() {
 	flag.Parse()
 	msg := flag.Arg(0)
 	rcp, _ := strconv.Atoi(flag.Arg(1))
 	time, _ := strconv.Atoi(flag.Arg(2))
 	numСhannels, _ := strconv.Atoi(flag.Arg(3))
-	arr := make([]chan Token, numСhannels, 2*numСhannels)
+	arr := make([]chan *Token, numСhannels, 2*numСhannels)
 	Tok := Token{msg, rcp, time}
-	first_channel := make(chan Token)
-	arr[0] = first_channel
-	createChannels(numСhannels, arr)
 
-	first_channel <- Tok
+	for i := 0; i < numСhannels; i++ {
+		arr[i] = make(chan *Token)
+	}
+
+	go node(arr[numСhannels-1], arr[0], numСhannels)
+
+	for i := numСhannels - 2; i > 1; i-- {
+		go node(arr[i], arr[i+1], i)
+	}
+
+	go node(arr[0], arr[1], 0)
+	go node(arr[1], arr[2], 1)
+	arr[0] <- &Tok
 }
-func node(in <-chan Token, out chan<- Token, num int) { // подумать над num
+
+func node(in <-chan *Token, out chan<- *Token, num int) {
 	selfNum := num
-	Tok := <-in
-	Tok.ttl -= 1
-	if Tok.recipient == selfNum {
-		fmt.Println("Token is delivered", Tok.data)
-		close(out)
-		return
-	}
-	if Tok.ttl == 0 {
-		fmt.Println("Token died")
-		close(out)
-		return
-	}
-	out <- Tok
-}
-
-func createChannels(numСhannels int, arr []chan Token) {
-	for i := 1; i < numСhannels; i++ {
-		if i == 1 {
-			arr[1] = make(chan Token)
-			go node(arr[0], arr[1], 1)
-		} else if i == numСhannels-1 {
-			go node(arr[i], arr[0], i)
-		} else {
-			arr[i+1] = make(chan Token)
-			go node(arr[i], arr[i+1], i)
+	for {
+		Tok := <-in
+		println(selfNum)
+		Tok.ttl -= 1
+		if Tok.recipient == selfNum {
+			fmt.Print("Token is delivered to ")
+			fmt.Println(Tok.recipient)
+			fmt.Println(Tok.data)
+			return
+		}
+		if Tok.ttl < 0 {
+			fmt.Println("Token died")
+			return
+		}
+		if out != nil {
+			out <- Tok
 		}
 	}
 }
